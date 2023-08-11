c50279bade272845d66cc88c2a7bb2001bcdb43a
Shreya Singh <shreyasingh200297.ss@gmail.com>
2023-08-11 13:06:00 -0400

    Added view boilerplate and firebase

---
+++ Lessgoo/Views/DestinationDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DestinationDetailView.swift
+//  Lessgoo
+//
+//  Created by Aerologix Aerologix on 8/11/23.
+//
+
+import SwiftUI
+
+struct DestinationDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DestinationDetailView()
+}

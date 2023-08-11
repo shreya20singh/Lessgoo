2ec064be69abbb8e6ed1da0acc9fa4be30d599cf
Shreya Singh <shreyasingh200297.ss@gmail.com>
2023-08-11 12:44:06 -0400

    proj setup

---
+++ Lessgoo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lessgoo
+//
+//  Created by Aerologix Aerologix on 8/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Lessgoo/LessgooApp.swift
@@ -0,0 +1,17 @@
+//
+//  LessgooApp.swift
+//  Lessgoo
+//
+//  Created by Aerologix Aerologix on 8/11/23.
+//
+
+import SwiftUI
+
+@main
+struct LessgooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
